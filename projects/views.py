360f9c4e4bd2513e8573a5ffce987a5f3bd22902
Neokairos <elielkenshi@gmail.com>
2023-08-20 12:04:15 -0300

    Edit Note added

--- projects/views.py
+++ projects/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from .forms import NoteForm
 from datetime import datetime
 from .models import Note
+from .forms import NoteForm
 
 
 def home(request):
@@ -28,7 +29,7 @@ def signupView(request):
             messages.success(request, 'You have successfully signed in!')
             return redirect(reverse('home'))
         else:
-            messages.error(request, 'Login failed. Please correct the errors below.')
+            messages.error(request, 'Registration failed. Please correct the errors below.')
 
     else: 
         form = UserCreationForm()
@@ -78,4 +79,16 @@ def delete_note(request, note_id):
         note.delete()
     return redirect('notes')
 
+
+def edit_note(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    if request.method == 'POST':
+        form = NoteForm(request.POST,instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('notes')
+        
+    else:
+        NoteForm(instance=note)
+    return render(request, 'edit_note.html', {'form':form, 'note':note})
 
