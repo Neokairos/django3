d4c35215557e99203b65edcd4f611874b65d1bd4
Neokairos <elielkenshi@gmail.com>
2023-08-24 16:58:30 -0300

    Added unit testing

--- projects/tests.py
+++ projects/tests.py
@@ -1,14 +1,14 @@
 from django.test import TestCase
 from .models import Note
 
-class SearchTest(TestCase):
+class SearchTestCase(TestCase):
     def setUp(self):
         self.note1 = Note.objects.create(description="this is a note", author = "Note Author")
         self.note2 = Note.objects.create(description="this is another note", author = "Another Note Author")
 
-    def search_notes_on_notes(self):
-        response = self.client.get('/notes/', {'search_query':'test'})
+    def test_search_notes_on_notes(self):
+        response = self.client.get('/notes', {'search_query':'test'})
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'This is a test note.')
-        self.assertContains(response, 'Another test note.')
+        self.assertContains(response, 'this is a note.')
+        self.assertContains(response, 'this is another note.')
 
